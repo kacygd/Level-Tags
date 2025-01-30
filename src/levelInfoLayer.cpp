ed45deaea9a4c52a85dce1a5b30c06aaca35fe84
kacygd <164707129+kacygd@users.noreply.github.com>
2025-01-30 09:50:16 +0700

    Update levelInfoLayer.cpp

--- src/levelInfoLayer.cpp
+++ src/levelInfoLayer.cpp
@@ -61,7 +61,7 @@ class $modify(TagsLevelInfoLayer, LevelInfoLayer) {
         });
 
         auto req = web::WebRequest();
-        m_fields->m_listener.setFilter(req.get(fmt::format("https://cps.ps.fhgdps.com/v1/{}.json", m_level->m_levelID.value())));
+        m_fields->m_listener.setFilter(req.get(fmt::format("https://cps.ps.fhgdps.com/v1/tags/{}.json", m_level->m_levelID.value())));
     };
 
     CCMenu* createTagContainer() {
